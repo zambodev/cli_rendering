84eb2499938fcda870361672d1508078936be112
zambodev <thomasz01@outlook.it>
2023-03-10 11:39:27 +0100

    Fixed bug

--- main.c
+++ main.c
@@ -10,47 +10,50 @@
 
 typedef struct VEC2
 {
-	int x[SIZE];
-	int y[SIZE];
-	int fill_idx;
+	int x[SIZE*SIZE];
+	int y[SIZE*SIZE];
+	int h[SIZE*SIZE];
+	int f;
 } vec2;
 
 int vec2_find(vec2 *points, int x, int y)
 {
-	for(int i=0; i<SIZE; ++i)
+	for(int i=0; i<SIZE*SIZE; ++i)
 		if(points->x[i] == x && points->y[i] == y)
-			return 1;
+			return points->h[i];
 
 	return 0;
 }
 
-void vec2_insert(vec2 *points, int x, int y)
+void vec2_insert(vec2 *points, int x, int y, int h)
 {
-	if(vec2_find(points, x, y) || points->fill_idx == SIZE) return;
+	if(vec2_find(points, x, y) || points->f == SIZE*SIZE) return;
 
-	points->x[points->fill_idx] = x;	
-	points->y[points->fill_idx] = y;	
+	points->x[points->f] = x;	
+	points->y[points->f] = y;
+	points->h[points->f] = h;	
 
-	++points->fill_idx;
+	++points->f;
 }
 
-void vec2_print(vec2 *points, size_t size, int setgraph)
+void vec2_print(vec2 *points, int setgraph)
 {	
 	if(setgraph)
 	{
 		printf("POINT VIEW:\n");
 		printf("|-");
-		for(int i=0; i<size; ++i)
+		for(int i=0; i<SIZE; ++i)
 			printf("--");
 		printf("|\n");
 
-		for(int i=0; i<size; ++i)
+		for(int i=0; i<SIZE; ++i)
 		{
 			printf("| ");
-			for(int j=0; j<size; ++j)
+			for(int j=0; j<SIZE; ++j)
 			{	
-				if(vec2_find(points, j, i))
-					printf("2 ");
+				int tmp;
+				if((tmp = vec2_find(points, j, i)))
+					printf("%d ", tmp);
 				else
 					printf("  ");
 			}
@@ -59,41 +62,41 @@ void vec2_print(vec2 *points, size_t size, int setgraph)
 		}
 		
 		printf("|-");
-		for(int i=0; i<size; ++i)
+		for(int i=0; i<SIZE; ++i)
 			printf("--");
 		printf("|\n");
 	}
 	else
 	{
-		for(int i=0; i<SIZE; ++i)
+		for(int i=0; i<SIZE*SIZE; ++i)
 			printf("%d %d\n", points->x[i], points->y[i]);	
 	}
 
 	printf("\n");
 }
 
-void map_load(char map[], size_t size, const char *filename)
+void map_load(char map[], const char *filename)
 {
 	FILE *f = fopen(filename, "r");
-	fread(map, 1, size*size, f);
+	fread(map, 1, SIZE*SIZE, f);
 	fclose(f);	
 }
 
-void map_print(char map[], size_t size, int setzero)
+void map_print(char map[], int setzero)
 {
 	printf("MAP VIEW:\n");
 	printf("|-");
-	for(int i=0; i<size; ++i)
+	for(int i=0; i<SIZE; ++i)
 		printf("--");
 	printf("|\n");
 
-	for(int i=0; i<size; ++i)
+	for(int i=0; i<SIZE; ++i)
 	{
 		printf("| ");
-		for(int j=0; j<size; ++j)
+		for(int j=0; j<SIZE; ++j)
 		{
-			if(setzero || map[i*size+j] != '0')
-				printf("%c ", map[i*size+j]);
+			if(setzero || map[i*SIZE+j] != '0')
+				printf("%c ", map[i*SIZE+j]);
 			else
 				printf("  ");	
 		}
@@ -102,20 +105,20 @@ void map_print(char map[], size_t size, int setzero)
 	}
 	
 	printf("|-");
-	for(int i=0; i<size; ++i)
+	for(int i=0; i<SIZE; ++i)
 		printf("--");
 	printf("|\n");
 }
 
 /* Get visible pointes */
-void map_getvp(char map[], size_t size, vec2 *points, double cam[2], double vangle)
+void map_getvp(char map[], vec2 *points, double cam[2], double vangle)
 {
-	for(int i=0; i<size*size; ++i)
+	for(int i=0; i<SIZE*SIZE; ++i)
 	{	
 		if(map[i] == '1')
 		{
-			cam[0] = i-((i/size)*size);	// X 
-			cam[1] = i/size;	// Y 
+			cam[0] = i-((i/SIZE)*SIZE);	// X 
+			cam[1] = i/SIZE;	// Y 
 		}
 	}
 	
@@ -128,12 +131,12 @@ void map_getvp(char map[], size_t size, vec2 *points, double cam[2], double vang
 			x += cos(i*PI/180); 
 			y -= sin(i*PI/180);
 		}
-		while(map[(int)y*size+(int)x] == '0' && x >= 0 && x < size && y >= 0 && y < size);
+		while(map[(int)y*SIZE+(int)x] <= '1' && x >= 0 && x < SIZE && y >= 0 && y < SIZE);
 
-		if(map[(int)y*size+(int)x] == '2')
+		if(map[(int)y*SIZE+(int)x] > '1' && map[(int)y*SIZE+(int)x] < '9')
 		{
-			vec2_insert(points, (int)x, (int)y);	
-			//printf("%d -- %fx %fy -- %c\n", (int)i, x, y, map[(int)y*size+(int)x]);
+			vec2_insert(points, (int)x, (int)y, map[(int)y*SIZE+(int)x]-48);	
+			//printf("%.2f -- %.2fx %.2fy -- %c\n", i, x, y, map[(int)y*SIZE+(int)x]);
 		}
 
 	}
@@ -141,14 +144,14 @@ void map_getvp(char map[], size_t size, vec2 *points, double cam[2], double vang
 	
 }
 
-void render(vec2 *points, size_t size, double userp[2])
+void render(vec2 *points, double userp[2])
 {
-	char *matrix = calloc(size*size, 1);
-	memset(matrix, '0', size*size);
+	char *matrix = calloc(SIZE*SIZE, 1);
+	memset(matrix, '0', SIZE*SIZE);
 
-	for(int i=0; i<size; ++i)
+	for(int i=0; i<SIZE; ++i)
 	{
-		if(i < points->fill_idx)
+		if(i < points->f)
 		{
 			double d = sqrt((points->x[i]-userp[0])*(points->x[i]-userp[0]) + 
 							(points->y[i]-userp[1])*(points->y[i]-userp[1]));
@@ -156,10 +159,10 @@ void render(vec2 *points, size_t size, double userp[2])
 			//printf("%d %d\n", points->x[i], points->y[i]);
 
 			double tmp = 0;
-			while(tmp < d && (size-points->x[i])-tmp >= 0)
+			while(tmp < d && (SIZE-points->x[i])-tmp >= 0)
 			{
-				//printf("%f %f\n", (size-points->x[i])-tmp, tmp);
-				matrix[(int)((size-points->x[i])-tmp)*size+points->y[i]] = '2';
+				//printf("%f %f\n", (SIZE-points->x[i])-tmp, tmp);
+				matrix[(int)((SIZE-points->x[i])-tmp)*SIZE+points->y[i]] = points->h[i];
 
 				tmp += 1.0;
 			}
@@ -167,7 +170,7 @@ void render(vec2 *points, size_t size, double userp[2])
 		//puts("---------");
 	}
 
-	map_print(matrix, size, 0);
+	map_print(matrix, 0);
 
 	free(matrix);
 }
@@ -178,11 +181,12 @@ int main(int argc, char **argv)
 	double camera[2];
 	vec2 points;
 
-	memset(points.x, -1, SIZE*4);
-	memset(points.y, -1, SIZE*4);
-	points.fill_idx = 0;
+	memset(points.x, -1, SIZE*SIZE*4);
+	memset(points.y, -1, SIZE*SIZE*4);
+	memset(points.h, 0, SIZE*SIZE*4);
+	points.f = 0;
 
-	map_load(map, SIZE, "map.txt");
+	map_load(map, "map.txt");
 
 	double angle = strtod(argv[1], NULL);
 	double val = 1;
@@ -194,15 +198,16 @@ int main(int argc, char **argv)
 		printf("\x1b[H\x1b[2J");
 		fflush(stdout);
 		printf("%f\n", angle);
-		memset(points.x, -1, SIZE*4);
-		memset(points.y, -1, SIZE*4);
-		points.fill_idx = 0;
+		memset(points.x, -1, SIZE*SIZE*4);
+		memset(points.y, -1, SIZE*SIZE*4);
+		memset(points.h, 0, SIZE*SIZE*4);
+		points.f = 0;
 #endif
 
-		map_print(map, SIZE, 0);
-		map_getvp(map, SIZE, &points, camera, angle);
-		vec2_print(&points, SIZE, 1);
-		render(&points, SIZE, camera);
+		map_print(map, 0);
+		map_getvp(map, &points, camera, angle);
+		vec2_print(&points, 1);
+		//render(&points, camera);
 
 #ifdef SHOWOFF
 		angle -= val;
